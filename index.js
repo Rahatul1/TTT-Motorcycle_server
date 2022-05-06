ac93d332e1c97d108b682b10dbe9d225f4749b8f
Rahatul <rahatulislam9090@gmail.com>
2022-05-07 01:30:38 +0600

    get items

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
 
-//middlewerw
+// middleware
 app.use(cors());
 app.use(express.json());
 
+//------------------//
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fozpl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 
-// run().catch(console.dir);
-app.get('/', (req, res) => {
-    res.send('Running warehouse or inventory management website')
-};
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client
+      .db("warehouseManagement")
+      .collection("produts");
+
+    //--get---//
+    app.get("/produts", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const produts = await cursor.toArray();
+      res.send(produts);
+    });
+    //
+    
+  } finally {
+    //
+  }
+}
+
+run().catch(console.dir);
+
+//-------------------------//
+app.get("/", (req, res) => {
+  res.send("Running warehouse or inventory management website");
+});
+
 app.listen(port, () => {
-    console.log('listening on port')
+  console.log(`"CRUD server started" ${port}`);
 });
