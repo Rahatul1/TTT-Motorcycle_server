94826407b8be511facdf1a4f8f8f77cf1a8fee09
Rahatul <rahatulislam9090@gmail.com>
2022-05-08 14:13:31 +0600

    some change

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function validate(req, res, next) {
   next();
 }
 
-//------------------//
+//----uri--------------//
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fozpl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
